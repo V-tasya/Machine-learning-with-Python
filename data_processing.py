f2ed3870a9b13a099f0c871d1cadbe2296aa3703
Tasya <rabchinskayavita811@gmail.com>
2025-05-07 00:18:42 +0200

    Added training and testing models

--- data_processing.py
+++ data_processing.py
@@ -3,10 +3,19 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import math
 
 numerical_values = ['Duration_Years', 'Living_Cost_Index', 'Rent_USD', 'Visa_Fee_USD', 'Insurance_USD', 'Exchange_Rate']
 categorical_values = ['Country', 'City', 'University', 'Program', 'Level']
 target_attribute = 'Tuition_USD'
+models = {'ln': LinearRegression(), 'dt': DecisionTreeRegressor(random_state=42), 'svr': SVR()}
+result = {}
+
 
 def reader(file):
   data_frame = pd.read_csv(file)
@@ -21,15 +30,30 @@ def analizer(data_frame):
   common_transformer = ColumnTransformer(transformers=[('num', num_transformer, numerical_values), ('cat', cat_transformer, categorical_values)])
 
   preprocessing = Pipeline(steps=[('preprocessor', common_transformer)])
-  transformed_columns = preprocessing.fit_transform(columns_without_target)
-  print('Form of the processed data: ', transformed_columns.shape)
+  #transformed_columns = preprocessing.fit_transform(columns_without_target)
+  #print('Form of the processed data: ', transformed_columns.shape)
 
-  return transformed_columns, target_variables_column
+  return columns_without_target, target_variables_column, preprocessing
+
+def training(columns_without_target, target_variables_column, preprocessing):
+  columns_without_target_train, columns_without_target_test, target_variables_column_train, target_variables_column_test = train_test_split(columns_without_target, target_variables_column, test_size=0.2, random_state=42)
+  
+  for id, model in models.items():
+    trainer = Pipeline([('processor', preprocessing), ('model', model)])
+    trainer.fit(columns_without_target_train, target_variables_column_train)
+    prediction = trainer.predict(columns_without_target_test)
+    mean_abs_er = mean_absolute_error(target_variables_column_test, prediction)
+    rout_mn_sqrt_er = math.sqrt(mean_squared_error(target_variables_column_test, prediction))
+
+    print('Model: ', id)
+    print('Mean average error: ', mean_abs_er)
+    print('Rout mean squared error: ', rout_mn_sqrt_er)
 
 def main():
   file = r'C:\Users\37529\PythonProjects\Project2\International_Education_Costs.csv'
   df = reader(file)
-  analizer(df)
+  columns, column, processor = analizer(df)
+  training(columns, column, processor)
 
 if __name__ == '__main__':
   main()
